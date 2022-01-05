f3a18ba909d7ed97f9ba1256b6acc80743d445fa
fernando tonezer frantz <fernandofrantz12@gmail.com>
2022-01-05 16:38:51 -0300

    problemas com decorators

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
 
-
 app = Flask(__name__)
+if (__name__ == '__main__'):
+    app.run()
 
 # Não altere essa configuração
 # Ela desabilita o sort automático dos JSONs por ordem alfabética
 app.config['JSON_SORT_KEYS'] = False
 
 # Desenvolva suas rotas abaixo
+
+#Rota de login
+#Status Code: 200 - OK, 400 - Bad Request ou 401 - Unauthorized;
+
+def verify_keys(trusted_keys: list[str]):
+    return ''
+
+@app.post('/login')
+@verify_keys([])
+def login():
+    return ''
--- decorators.py
+++ decorators.py
@@ -1,1 +1,5 @@
 # Desenvolva seus decorators aqui
+
+#login
+def verify_keys(trusted_keys: list[str]):
+    return ''
